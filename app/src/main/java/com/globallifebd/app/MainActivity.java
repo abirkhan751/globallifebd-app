7166190070dc53349d46ac3da28f42624f1df5d7
abirkhan751 <hasanjamilafnan099@gmail.com>
2026-09-14 20:28:20 +0600

    Release v1.0.6: WebP image acceleration, sidebar scroll fix, and version bump

--- app/src/main/java/com/globallifebd/app/MainActivity.java
+++ app/src/main/java/com/globallifebd/app/MainActivity.java
@@ -259,7 +259,7 @@ public class MainActivity extends AppCompatActivity {
         settings.setDisplayZoomControls(false);
         settings.setCacheMode(WebSettings.LOAD_DEFAULT);
 
-        String customUserAgent = settings.getUserAgentString() + " GlobalLifeBDApp/1.0.5";
+        String customUserAgent = settings.getUserAgentString() + " GlobalLifeBDApp/1.0.6";
         settings.setUserAgentString(customUserAgent);
 
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
