537c3faf06fe2697b3ea94219e82110b10e545cc
abirkhan751 <hasanjamilafnan099@gmail.com>
2026-09-14 10:46:10 +0600

    Bump version to 1.0.2 (versionCode 3) with sidebar, splash, and v1.0.2 release

--- app/src/main/java/com/globallifebd/app/MainActivity.java
+++ app/src/main/java/com/globallifebd/app/MainActivity.java
@@ -237,7 +237,7 @@ public class MainActivity extends AppCompatActivity {
         settings.setDisplayZoomControls(false);
         settings.setCacheMode(WebSettings.LOAD_DEFAULT);
 
-        String customUserAgent = settings.getUserAgentString() + " GlobalLifeBDApp/1.0.1";
+        String customUserAgent = settings.getUserAgentString() + " GlobalLifeBDApp/1.0.2";
         settings.setUserAgentString(customUserAgent);
 
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
