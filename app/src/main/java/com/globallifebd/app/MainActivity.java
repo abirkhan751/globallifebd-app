3a1dc6421d10afa608e24292dd2283509703c9dc
abirkhan751 <hasanjamilafnan099@gmail.com>
2026-09-15 16:17:00 +0600

    Add missing ImageView import in MainActivity

--- app/src/main/java/com/globallifebd/app/MainActivity.java
+++ app/src/main/java/com/globallifebd/app/MainActivity.java
@@ -37,6 +37,7 @@ import android.webkit.WebView;
 import android.webkit.WebViewClient;
 import android.widget.Button;
 import android.widget.FrameLayout;
+import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.TextView;
 import android.widget.Toast;
